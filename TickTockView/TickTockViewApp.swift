ef687c7e3eb6983cb3e385b57469fe00b4969190
Da Wei Hao <dawei84@hotaileasing.com.tw>
2025-09-11 15:36:52 +0800

    Initial Commit

---
+++ TickTockView/TickTockViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTockViewApp.swift
+//  TickTockView
+//
+//  Created by Da Wei Hao on 2025/9/11.
+//
+
+import SwiftUI
+
+@main
+struct TickTockViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TickTockViewTests/TickTockViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  TickTockViewTests.swift
+//  TickTockViewTests
+//
+//  Created by Da Wei Hao on 2025/9/11.
+//
+
+import Testing
+@testable import TickTockView
+
+struct TickTockViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
